7931abaf14eabb45153e79e0264ed6bcb18b4e32
Wil Cooley <wcooley@pdx.edu>
2026-04-02 12:55:03 -0700

    Update help docstring & README

--- src/templateer.py
+++ src/templateer.py
@@ -6,7 +6,36 @@ in the environment:
 
     VAR1=foo templateer -t example-with-var1.txt -o output.txt
 
-If not provided in the environment, templateer will prompt the user for values.
+Or in an env/ini-without-sections file:
+
+    templateer -i vars.ini -t example.tpl -o output.txt
+
+Or edited interactively:
+
+    templateer --edit -t example.tpl -o output.txt
+
+Or all three combined:
+
+    VAR1=foo templateer --edit -i vars.ini -t example.tpl -o output.txt
+
+If `--edit` is not provided, the user will be prompted for values of variables
+not provided in the environment or an input file.
+
+Variables in a template can be listed with `--list`.
+
+Variables are sorted for prompt and edit modes and can begin with a digit.
+
+NB: use the `env` command to provide variables starting with a digit as
+environment variables; otherwise some shells do not recognize the prefixed
+variable assignment. I.e., this works:
+
+    env 1_VAR=xxx templateer ...
+
+But this does not:
+
+    1_VAR=xxx templateer ...
+    -bash: 1_VAR=xxx: command not found
+
 """
 
 import argparse
